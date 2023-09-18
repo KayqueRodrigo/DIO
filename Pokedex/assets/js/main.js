4ba45ce88baccd73db733b88ed7928d449a20b4c
KAyqueRodrigo <kayque.frans@outlook.com>
2023-09-18 17:59:27 -0300

    Fazendo requiseção da pokeAPI e filtrando os campos que realmente preciso

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,32 @@
-console.log('Sucesso!');
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <ol class="pokemons" ${pokemon.type}>
+        <li class="pokemon">
+            <span class="namber">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+
+            </div>
+        </li>
+    </ol>
+    `
+}
+
+    //Pangando a lista no arquivo HTML atravas do document que a referencia
+    const pokemonList = document.getElementById('pokemonList')
+
+    //Fizemos a requisição //Resebemos essa requisão
+    pokeApi.getPokemons().then((pokemons = []) => {
+        //Transformando em uma lista de html
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+        //O join() juntará todos os elementos da list em uma string(Com o separador que colocarmos no parenteses e por defaul é',')
+        //const newHtml = newList.join('')
+    })
+    
+
